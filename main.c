48f6357f850242fd9134c8c9ece87e246c44f54e
Chapron Maxime <70152064+Maxwap@users.noreply.github.com>
2023-11-16 10:12:31 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "toDoList.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ toDoList.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef TP_TODOLIST_H
+#define TP_TODOLIST_H
+
+#endif //TP_TODOLIST_H
